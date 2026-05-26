15eee5accc2b890c89144881d9539fd00e8635a3
LIKYWAY3 <nguyenhuynhkha0203@gmail.com>
2026-05-26 19:25:48 +0700

    up

---
+++ ASPtestShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPtestShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
