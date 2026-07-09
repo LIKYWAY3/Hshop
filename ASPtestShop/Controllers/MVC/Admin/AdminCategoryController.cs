74bd36d4648ecebab01fb24d028fff725c6ae238
LIKYWAY3 <nguyenhuynhkha0203@gmail.com>
2026-07-10 00:24:14 +0700

    add mvc admin product and category views with product image upload

---
+++ ASPtestShop/Controllers/MVC/Admin/AdminCategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPtestShop.Controllers.MVC.Admin
+{
+    [Route("admin/categories")]
+    public class AdminCategoryController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPtestShop/Controllers/MVC/Admin/AdminProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPtestShop.Controllers.MVC.Admin
+{
+    [Route("admin/products")]
+    public class AdminProductController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
